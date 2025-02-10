796ee8941d7492be84784e6cc0588f12f16590d8
baichom1 <baichom1@mskcc.org>
2025-02-10 13:42:04 -0500

    fixing the name ordering

--- R/utilities_plotting.R
+++ R/utilities_plotting.R
@@ -1,14 +1,14 @@
 # Currently this is just hard coded, ideally would read in from postgres:
 base_colors = c(other="#BBBBBB",
-                    p__Actinobacteria = "#A77097", p__Bacteroidetes = "#51AB9B", p__Proteobacteria = "#AB3535", # "#FF0000". Ying had used full red spectrum. I used DD0000 to increase spectrum.
-                    o__Clostridiales = "#9C854E",
-                    o__Erysipelotrichales = "#7A6920", #o__Erysipelotrichales = "#ACA54D", #Erysipelotrichales order has been added mainly due to high frequency of genus: Erysipelatoclostridium.
-                    f__Lachnospiraceae = "#EC9B96", f__Ruminococcaceae = "#FF7106" ,#"#9AAE73",
-                    g__Enterococcus = "#129246", g__Lactobacillus = "#3B51A3", g__Staphylococcus = "#F1EB25", g__Streptococcus = "#9FB846",
-                    g__Escherichia = "#800026", g__Klebsiella = "#FF2211",
-                    g__Akkermansia = "#377EB8");
+                p__Actinobacteria = "#A77097", p__Bacteroidetes = "#51AB9B", p__Proteobacteria = "#AB3535", # "#FF0000". Ying had used full red spectrum. I used DD0000 to increase spectrum.
+                o__Clostridiales = "#9C854E",
+                o__Erysipelotrichales = "#7A6920", #o__Erysipelotrichales = "#ACA54D", #Erysipelotrichales order has been added mainly due to high frequency of genus: Erysipelatoclostridium.
+                f__Lachnospiraceae = "#EC9B96", f__Ruminococcaceae = "#FF7106" ,#"#9AAE73",
+                g__Enterococcus = "#129246", g__Lactobacillus = "#3B51A3", g__Staphylococcus = "#F1EB25", g__Streptococcus = "#9FB846",
+                g__Escherichia = "#800026", g__Klebsiella = "#FF2211",
+                g__Akkermansia = "#377EB8");
 
-taxa_order = c(phylum="p", class="c", order="o", family="f", genus="g")
+taxa_order = c(kingdom = "k", phylum="p", class="c", order="o", family="f", genus="g")
 
 #' This function will generate a color close to the original color comp provided
 #' 
@@ -16,7 +16,7 @@ taxa_order = c(phylum="p", class="c", order="o", family="f", genus="g")
 #' @param magnitude the maximum amount to adjust rgb component by (default 4)
 #' @export
 #' @name norm_generate_component
-norm_generate_component <- function(rgb_component, magnitude = 7){
+norm_generate_component <- function(rgb_component, magnitude = 15){
   rgb_component <- rgb_component + sample(-magnitude:magnitude, 1)
   rgb_component <- min(255, max(0, rgb_component))
 }
@@ -27,7 +27,6 @@ norm_generate_component <- function(rgb_component, magnitude = 7){
 #' @export
 #' @name blur_color
 blur_color <- function(color){
-  set.seed(123)
   rgb_comps <- col2rgb(color)
   return(rgb(
     norm_generate_component(rgb_comps[1]), 
@@ -35,7 +34,7 @@ blur_color <- function(color){
     norm_generate_component(rgb_comps[3]), 
     maxColorValue = 255, 
     names = NULL))
-
+  
 }
 
 
@@ -53,24 +52,38 @@ rename_taxa_colors <- function(palette, full_taxonomy, rank){
     dplyr::mutate(tax_level = gsub("(.*)__.*", "\\1", group))
   inds_to_adjust = rep(FALSE, length(palette))
   new_palette = unname(palette)
-  t_names = names(palette)
+  shuffled_names = rownames(full_taxonomy)
+  
+  # For all the taxa names, update the color and "blur" the colors from higher clades.
   for(tax_level in colnames(full_taxonomy) ){
-    ind_cur_tax_level = which(color_group_tax_info[,"tax_level"] == taxa_order[tax_level])
-    annotation_level_cur = full_taxonomy[tax_level]
-    for(i_tax_cur in ind_cur_tax_level ){
-      new_palette[inds_to_adjust] <- lapply(new_palette[inds_to_adjust], blur_color)
-      matching_taxonomy = annotation_level_cur == color_group_tax_info[i_tax_cur,"group"]
-      new_palette[matching_taxonomy] <- base_colors[color_group_tax_info[i_tax_cur,"group"]]
-      inds_to_adjust = apply(matrix(c(inds_to_adjust, matching_taxonomy), 2), 2, any) #update the inds to adjust vector by adding in those modified at this level
-      #rotate so that the values of each matchin taxonomy are adjacent:
-      new_palette <- c(new_palette[matching_taxonomy], new_palette[!matching_taxonomy])
-      t_names <- c(t_names[matching_taxonomy], t_names[!matching_taxonomy])
-      inds_to_adjust <- c(inds_to_adjust[matching_taxonomy], inds_to_adjust[!matching_taxonomy])
+    new_palette[inds_to_adjust] <- lapply(new_palette[inds_to_adjust], blur_color)
+    for(clade in unique(full_taxonomy[tax_level][[1]])){
+      if (clade %in% names(base_colors)){
+        new_palette[which(full_taxonomy[tax_level] == clade)] <- base_colors[clade]
+        inds_to_adjust = inds_to_adjust | full_taxonomy[tax_level] == clade
+        s_index <- grepl(clade, shuffled_names)
+        shuffled_names <- c(shuffled_names[s_index], shuffled_names[!s_index])
+      }
     }
-  } 
+  }
+  
+  #This will swap around the order of the names so that groups of related genera are together in the palette:
+  ordered_palette <- data.frame(
+    color = list_c(new_palette),
+    name = full_taxonomy[rank][[1]],
+    full_tax = rownames(full_taxonomy)
+  ) %>% left_join(data.frame(
+    full_tax = shuffled_names,
+    order_index = seq(1, length(palette))
+  )) %>%
+    arrange(order_index)
+  
+  # Make the new palette and add in the "other" color:
+  new_palette <- ordered_palette$color
   new_palette <- c(new_palette, "lightgrey")
   new_palette <- as.character(new_palette)
-  names(new_palette) <- c(names(palette), "Other")
+  
+  names(new_palette) <- c(ordered_palette$name, "Other")
   return(new_palette)
 }
 
@@ -94,19 +107,19 @@ make_microviz_palette <- function(phy_seq_obj, n, rank){
       return(NULL)
     }
   }
-
+  
   #make the default palette based on phyloseq object and params:
   my_palette <- microViz::tax_palette(phy_seq_obj, n=n, rank=rank, add = NA) #get a palette with n/rank
   index_rank <- grep(rank, colnames(phyloseq::tax_table(phy_seq_obj)))
-
+  
   taxonomy <- phy_seq_obj %>%
     phyloseq::tax_table() %>%
     as.data.frame() %>%
     dplyr::select(1:all_of(index_rank)) %>%
     dplyr::filter(.[[index_rank]] %in% names(my_palette)) %>%
     unique()
-
+  
   #rename colors using basenames:
   return(rename_taxa_colors(my_palette, taxonomy, rank))
-
+  
 }
